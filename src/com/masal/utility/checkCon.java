f2ffea741bdb3a26fa3e098e9d9743974373af3d
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-09-27 23:29:07 +0530

    DB connection class

---
+++ src/com/masal/utility/checkCon.java
@@ -0,0 +1,28 @@
+package com.masal.utility;
+
+import java.sql.Connection;
+
+public class checkCon {
+	
+
+
+		public static void main(String[] args) {
+			
+			Connection con=DButil.getConnection();
+			
+			if(con!=null) {
+				System.out.println("connected");
+			}
+			else {
+				System.out.println("not");
+			}
+			
+
+		}
+
+
+	
+	
+	
+
+}
